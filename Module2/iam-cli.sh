319c39160bded43cefdadea8a6240092027cb8d8
www42 <thomas.jaekel@gfn.training>
2022-07-18 00:14:43 +0200

    Add more aws iam commands

--- Module2/iam-cli.sh
+++ Module2/iam-cli.sh
@@ -1,5 +1,7 @@
 aws --version
 
+aws sts get-caller-identity
+
 # https://awscli.amazonaws.com/v2/documentation/api/latest/reference/iam/index.html
 aws iam list-users
 aws iam list-users --query 'Users[].UserName'
@@ -10,7 +12,15 @@ aws iam list-users --query 'Users[].{UserName:UserName,UserId:UserId}' --output 
 aws iam list-users --query 'Users[].{UserName:UserName,UserId:UserId}' --output yaml
 
 aws iam list-groups
+
 aws iam list-roles
+aws iam list-roles --query 'Roles[].RoleName'
+
+# https://aws.amazon.com/premiumsupport/knowledge-center/iam-assume-role-cli/
+aws iam get-role --role-name MyS3AdminRole
+aws iam list-attached-role-policies --role-name MyS3AdminRole
+
+
 aws iam list-policies
 aws iam list-policies --query 'length(Policies)'
 aws iam list-policies --max-items 3
