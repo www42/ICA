4103156054e1cd98e871e317182c57ffdaee52d0
www42 <thomas.jaekel@gfn.training>
2022-09-21 15:42:13 +0200

    Lab 1 ready

--- Lab_1/Lab1_Task4.2_CLI.sh
+++ Lab_1/Lab1_Task4.2_CLI.sh
@@ -4,3 +4,7 @@ myCliBucket='labclibucket-69118'
 aws s3 mb s3://$myCliBucket
 aws s3 cp /home/ssm-user/HappyFace.jpg s3://$myCliBucket
 aws s3 ls s3://$myCliBucket
+
+
+aws s3 rm s3://$myCliBucket/HappyFace.jpg   # Access denied -not allowed in Lab environment
+aws s3 rb s3://$myCliBucket --force         # Access denied -not allowed in Lab environment
